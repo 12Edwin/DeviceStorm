fb804f8ce9debb152c562a95d0bd94390ac0d38a
Edwin <20213tn046@utez.edu.mx>
2023-12-03 20:06:36 -0600

    Supplier and Place services

---
+++ Services/src/module/supplier/supplier.controller.js
@@ -0,0 +1,93 @@
+const {Router} = require("express");
+const Supplier = require('./Supplier');
+const {validateError, validateMiddlewares} = require("../../util/functions");
+const {validateJWT, validateIdSupplier} = require("../../helpers/db-validations");
+const {check} = require("express-validator");
+
+const getAll = async (req, res= Response) =>{
+    try {
+        const query = req.query;
+        const [suppliers,total] = await Promise.all([
+            Supplier.find(query),
+            Supplier.countDocuments(query)
+        ]);
+
+        res.status(200).json({msg:'Successful request', total, suppliers});
+    }catch (error){
+        const message = validateError(error);
+        res.status(400).json(message);
+        console.log(error);
+    }
+}
+
+const getById = async (req, res= Response) =>{
+    try {
+        const {id} = req.params;
+        const supplier = await Supplier.findById(id);
+
+        res.status(200).json({msg:'Successful request', supplier});
+    }catch (error){
+        const message = validateError(error);
+        res.status(400).json(message);
+        console.log(error);
+    }
+}
+
+const insert = async (req, res= Response) =>{
+    try {
+        const {name, direction, contact} = req.body
+        const created_at = new Date()
+        const supplier = await new Supplier({name, direction, contact, created_at, status: true});
+        await supplier.save();
+
+        res.status(200).json({msg:'Successful request', supplier});
+    }catch (error){
+        const message = validateError(error);
+        res.status(400).json(message);
+        console.log(error);
+    }
+}
+
+const deletes = async (req, res= Response) =>{
+    try {
+        const {id} = req.params;
+        await Supplier.findByIdAndDelete(id);
+
+        res.status(200).json({msg:'Status deleted'});
+    }catch (error){
+        const message = validateError(error);
+        res.status(400).json(message);
+        console.log(error);
+    }
+}
+
+const supplierRouter = Router();
+
+supplierRouter.get('/',[
+    validateJWT
+],getAll);
+
+supplierRouter.get('/:id',[
+    validateJWT,
+    check('id', 'El id debe ser de mongo').isMongoId(),
+    check('id').custom(validateIdSupplier),
+    validateMiddlewares
+],getById);
+
+supplierRouter.post('/',[
+    validateJWT,
+    check('name', 'Name is required').not().isEmpty(),
+    check('direction').not().isEmpty().withMessage('Direction is required'),
+    check('contact').not().isEmpty().withMessage('Contact is required'),
+    check('contact').isNumeric().withMessage('Contact must be number'),
+    validateMiddlewares
+], insert);
+
+supplierRouter.delete('/:id',[
+    validateJWT,
+    check('id', 'El id debe ser de mongo').isMongoId(),
+    check('id').custom(validateIdSupplier),
+    validateMiddlewares
+],deletes);
+
+module.exports = { supplierRouter }
