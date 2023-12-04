f1662f7ccb3e6b608f380b2c85cb2da58a14c87b
Edwin <20213tn046@utez.edu.mx>
2023-12-03 20:58:40 -0600

    [FIX] validate request to insert device

--- Services/src/module/device/device.controller.js
+++ Services/src/module/device/device.controller.js
@@ -2,7 +2,7 @@ const {Router} = require("express");
 const {validateError, validateMiddlewares} = require("../../util/functions");
 const Device = require ('./Device');
 const {check} = require("express-validator");
-const {validateJWT, validateIdDevice, existDevice} = require("../../helpers/db-validations");
+const {validateJWT, validateIdDevice, existDevice, validateIdPlace, validateIdSupplier, validateIdCategory} = require("../../helpers/db-validations");
 const multer = require('multer');
 const path = require('path');
 const fs = require("fs");
@@ -38,8 +38,9 @@ const getById = async (req, res = Response) =>{
 
 const insert = async (req, res = Response) =>{
     try {
-        const {name, author, publication, resume, category, price} = req.body;
-        const device = await new Device({name, author, publication, resume, category, price, status:true});
+        const {name, code, place, supplier, category, stock} = req.body;
+        const created_at = new Date();
+        const device = await new Device({name, code, place, supplier, category, stock, created_at, available:true});
         await device.save();
 
         res.status(200).json({message:'Successful request', device});
@@ -137,12 +138,20 @@ deviceRouter.get('/:id',[
 
 deviceRouter.post('/',[
     validateJWT,
-    check('name', 'El titulo del libro es obligatorio').not().isEmpty(),
+    check('name', 'Name is required').not().isEmpty(),
     check('name').custom(existDevice),
-    check('author', 'El autor del libro es obligatorio').not().isEmpty(),
-    check('publication', 'La fecha de publicación es obligatoria').not().isEmpty(),
-    check('publication').trim().isDate().withMessage('Must be a valid date'),
-    check('price').not().isEmpty().withMessage('El precio es obligatorio'),
+    check('code').isString().not().isEmpty().withMessage('Code is required'),
+    check('place').not().isEmpty().withMessage('Place is required'),
+    check('place').isMongoId().withMessage('Invalid place'),
+    check('place').custom(validateIdPlace).withMessage('Invalid place'),
+    check('supplier', 'Supplier is required').not().isEmpty(),
+    check('supplier').isMongoId().withMessage('Invalid supplier'),
+    check('supplier').custom(validateIdSupplier).withMessage('Invalid supplier'),
+    check('category').not().isEmpty().withMessage('Category is required'),
+    check('category').isMongoId().withMessage('Invalid category'),
+    check('category').custom(validateIdCategory).withMessage('Invalid category'),
+    check('stock').not().isEmpty().withMessage('Stock is required'),
+    check('stock').isNumeric().withMessage('Stock must be numeric'),
     validateMiddlewares
 ],insert);
 
